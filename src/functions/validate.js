d7e1130f528849abac9c2f5f3ea0b9c161b34e34
Arthur Bruno <arthur.abbrr@gmail.com>
2022-06-22 14:58:26 -0300

    creating validation functions

---
+++ src/functions/validate.js
@@ -0,0 +1,16 @@
+export default {
+  isDataValid: (questions, step, data) => {
+
+    const question = questions[step];
+
+    if (data.rating === null) {
+      return { error: 'Ocorreu um erro inesperado.' };
+    }
+
+    if (question.textField && question.textField.required && !data.textField) {
+      return { error: '*Obrigatório' };
+    }
+
+    return true;
+  }
+};
