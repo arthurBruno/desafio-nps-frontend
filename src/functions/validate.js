9578a21ac72f92f0e5f66809df205f079162f9f2
Arthur Bruno <arthur.abbrr@gmail.com>
2022-06-22 15:04:45 -0300

    verifying data

--- src/functions/validate.js
+++ src/functions/validate.js
@@ -3,11 +3,11 @@ export default {
 
     const question = questions[step];
 
-    if (data.rating === null) {
+    if (data?.rating === null) {
       return { error: 'Ocorreu um erro inesperado.' };
     }
 
-    if (question.textField && question.textField.required && !data.textField) {
+    if (question.textField && question.textField.required && !data?.textField) {
       return { error: '*Obrigatório' };
     }
 
